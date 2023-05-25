4dbeb5f8735b5c7d02cb21ea570ece72b5735080
Seif Wael <seifeldin7666@gmail.com>
2023-05-25 13:54:41 +0300

    Prinintg Model

--- pcdet/models/backbones_3d/spconv_backbone.py
+++ pcdet/models/backbones_3d/spconv_backbone.py
@@ -140,7 +140,7 @@ class VoxelBackBone8x(nn.Module):
         voxel_features, voxel_coords = batch_dict['voxel_features'], batch_dict['voxel_coords']
         batch_size = batch_dict['batch_size']
         input_sp_tensor = spconv.SparseConvTensor(
-            features=voxel_features.cuda(),
+            features=voxel_features,
             indices=voxel_coords.int(),
             spatial_shape=self.sparse_shape,
             batch_size=batch_size
