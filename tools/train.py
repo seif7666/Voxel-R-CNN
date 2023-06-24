8a6530f06ec88018c63f8d715bd5820617a61716
Seif Wael <seifeldin7666@gmail.com>
2023-06-24 16:07:08 +0300

    Remove Dist Train

--- tools/train.py
+++ tools/train.py
@@ -139,8 +139,8 @@ def main():
             last_epoch = start_epoch + 1
 
     model.train()  # before wrap to DistributedDataParallel to support fixed some parameters
-    if dist_train:
-        model = nn.parallel.DistributedDataParallel(model, device_ids=[cfg.LOCAL_RANK % torch.cuda.device_count()])
+    # if dist_train:
+    #     model = nn.parallel.DistributedDataParallel(model, device_ids=[cfg.LOCAL_RANK % torch.cuda.device_count()])
     logger.info(model)
 
     lr_scheduler, lr_warmup_scheduler = build_scheduler(
