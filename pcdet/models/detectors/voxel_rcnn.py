253ca590b14929c0647e69b642e0ae00ae729154
Seif Wael <seifeldin7666@gmail.com>
2023-06-24 16:08:28 +0300

    Remove Dist Train

--- pcdet/models/detectors/voxel_rcnn.py
+++ pcdet/models/detectors/voxel_rcnn.py
@@ -4,7 +4,7 @@ from .detector3d_template import Detector3DTemplate
 class VoxelRCNN(Detector3DTemplate):
     def __init__(self, model_cfg, num_class, dataset):
         super().__init__(model_cfg=model_cfg, num_class=num_class, dataset=dataset)
-        # self.module_list = self.build_networks()
+        self.module_list = self.build_networks()
 
     def forward(self, batch_dict):
         for cur_module in self.module_list:
