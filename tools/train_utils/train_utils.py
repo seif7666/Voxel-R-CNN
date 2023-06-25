a0357173c34b2782c3b8971ee8f0daf1e86ae21f
Seif Wael <seifeldin7666@gmail.com>
2023-06-25 18:22:12 +0300

    Trying again

--- tools/train_utils/train_utils.py
+++ tools/train_utils/train_utils.py
@@ -69,7 +69,7 @@ def train_model(model, optimizer, train_loader, model_func, lr_scheduler, optim_
                 merge_all_iters_to_one_epoch=False):
     accumulated_iter = start_iter
     # print(f'Length is {len(train_loader)}')
-    with tqdm.trange(start_epoch, total_epochs, desc='epochs', dynamic_ncols=False, leave=(rank == 0)) as tbar:
+    with tqdm.trange(start_epoch, total_epochs, desc='epochs', dynamic_ncols=True, leave=(rank == 0)) as tbar:
         total_it_each_epoch = len(train_loader)
         if merge_all_iters_to_one_epoch:
             assert hasattr(train_loader.dataset, 'merge_all_iters_to_one_epoch')
@@ -77,8 +77,7 @@ def train_model(model, optimizer, train_loader, model_func, lr_scheduler, optim_
             total_it_each_epoch = len(train_loader) // max(total_epochs, 1)
         
         dataloader_iter = iter(train_loader)
-        for cur_epoch in tbar:
-            print('x')
+        
         for cur_epoch in tbar:
             if train_sampler is not None:
                 train_sampler.set_epoch(cur_epoch)
