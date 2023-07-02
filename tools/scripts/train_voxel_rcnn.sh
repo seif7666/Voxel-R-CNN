28df73c269ab1075441938568856a98649569dfe
Seif Wael <seifeldin7666@gmail.com>
2023-07-02 22:44:31 +0300

    4th commit

--- tools/scripts/train_voxel_rcnn.sh
+++ tools/scripts/train_voxel_rcnn.sh
@@ -8,4 +8,4 @@ CFG_DIR=cfgs
 
 CFG_NAME=voxel_rcnn/voxel_rcnn_car
 
-python -m torch.distributed.launch --nproc_per_node=${NGPUS} train.py --launcher pytorch --cfg_file cfgs/$CFG_NAME.yaml --epochs 1 --workers 1 --pretrained_model "/content/drive/MyDrive/Graduation Project/output3/ckpt/checkpoint_epoch_1.pth"
+python -m torch.distributed.launch --nproc_per_node=${NGPUS} train.py --launcher pytorch --cfg_file cfgs/$CFG_NAME.yaml --epochs 1 --workers 2 --pretrained_model "/content/drive/MyDrive/Graduation Project/output3/ckpt/checkpoint_epoch_1.pth"
